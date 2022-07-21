36d4c3f6873b4dd927683dc7c6c3418518a29135
Forrest Lyman <forrest@dighub.io>
2022-07-21 12:23:25 -0700

    adds store integration testing to title list test

--- src/app/components/title-list/title-list.component.spec.ts
+++ src/app/components/title-list/title-list.component.spec.ts
@@ -13,13 +13,28 @@ import {MatListModule} from '@angular/material/list';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonHarness} from '@angular/material/button/testing';
-
+import {MockStore, provideMockStore} from '@ngrx/store/testing';
+import {initialState, titleReducer} from '../../store/title/title.reducer';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {ReactiveFormsModule} from '@angular/forms';
+import {selectMyTitles} from '../../store/title/title.selectors';
+import {first, firstValueFrom, interval, lastValueFrom, take, takeUntil, takeWhile} from 'rxjs';
+import {map, tap} from 'rxjs/operators';
+import {Title} from '../../store/title/title.interfaces';
+import {Store, StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {EffectsModule} from '@ngrx/effects';
+import {TitleEffects} from '../../store/title/title.effects';
+import {TitleService} from '../../services/title.service';
+import {AppState} from '../../store/app';
+import {setMyTitles} from '../../store/title/title.actions';
 
 let loader: HarnessLoader;
 
 describe('TitleListComponent', () => {
   let component: TitleListComponent;
   let fixture: ComponentFixture<TitleListComponent>;
+  let store: Store<AppState>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -31,6 +46,14 @@ describe('TitleListComponent', () => {
         MatExpansionModule,
         MatIconModule,
         MatListModule,
+        MatSnackBarModule,
+        StoreModule.forRoot({
+          title: titleReducer
+        }, {}),
+        EffectsModule.forRoot([TitleEffects]),
+      ],
+      providers: [
+        TitleService
       ]
     })
     .compileComponents();
@@ -39,6 +62,10 @@ describe('TitleListComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
     loader = TestbedHarnessEnvironment.loader(fixture);
+    store = TestBed.inject(Store);
+    store.dispatch(setMyTitles({titles: [
+        ...titles.slice(0,3)
+      ]}));
   });
 
   it('should create', () => {
@@ -46,21 +73,25 @@ describe('TitleListComponent', () => {
   });
 
   it('should render a panel for each title', async () => {
-    component.titles = titles;
     fixture.detectChanges();
+
+    // find all of the panels
     const panels = await loader.getAllHarnesses(MatExpansionPanelHarness);
-    expect(panels.length).toEqual(50);
+
+    // confirm that there are an equal number of panels to myTitles
+    expect(panels.length).toEqual(3);
   });
 
-  it('should should fire the event when a title is removed', (done: DoneFn) => {
-    component.titles = titles;
+  it('should remove a title', async () => {
     fixture.detectChanges();
-    loader.getAllHarnesses(MatButtonHarness).then(buttons => {
-      component.removeTitle.subscribe(title => {
-        expect(title).toEqual(titles[0]);
-        done();
-      })
-      buttons[0].click();
-    });
+    // find the first button
+    const button = await loader.getHarness(MatButtonHarness);
+
+    // trigger the click event
+    await button.click();
+
+    // confirm that the title was removed
+    const res = await firstValueFrom(store.select(selectMyTitles));
+    expect(res.length).toEqual(2);
   });
 });
