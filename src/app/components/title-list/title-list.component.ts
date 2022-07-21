130a300a191c6fe7616df70e3b4a9be1a2b12b23
Forrest Lyman <forrest@dighub.io>
2022-07-21 07:55:52 -0700

    updates title list to interact directly with the store

--- src/app/components/title-list/title-list.component.ts
+++ src/app/components/title-list/title-list.component.ts
@@ -1,18 +1,32 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Title} from '../../store/title/title.interfaces';
+import {Observable, Subscription} from 'rxjs';
+import {FormControl} from '@angular/forms';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../store/app';
+import {addTitle, loadMyTitles, loadTitles, setFilter, saveMyTitles, removeTitle} from '../../store/title/title.actions';
+import {
+  selectFilteredTitles,
+  selectMyTitles, selectMyTitlesDirty,
+  selectTitles
+} from '../../store/title/title.selectors';
 
 @Component({
   selector: 'app-title-list',
   templateUrl: './title-list.component.html',
   styleUrls: ['./title-list.component.css']
 })
-export class TitleListComponent implements OnInit {
-  @Input() titles: Title[] | null = [];
-  @Output() removeTitle: EventEmitter<Title> = new EventEmitter<Title>();
+export class TitleListComponent {
+  public readonly myTitles$: Observable<Title[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private snackbar: MatSnackBar, private store: Store<AppState>) {
+    store.dispatch(loadMyTitles());
+    this.myTitles$ = store.select(selectMyTitles);
   }
 
+  removeTitle(title: Title) {
+    this.snackbar.open(`You removed ${title.name} from your list!`);
+    this.store.dispatch(removeTitle({id: title.id}))
+  }
 }
